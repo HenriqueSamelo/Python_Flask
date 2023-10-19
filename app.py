0261e00736ccc3741ec72b6dab3e58a0b8c50954
HenriqueSamelo <133215063+HenriqueSamelo@users.noreply.github.com>
2023-10-19 11:44:46 -0300

    First

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import urllib.request, json
 
 app = Flask(__name__)
 
@@ -21,6 +22,24 @@ def Sobre():
 		if request.form.get("aluno") and request.form.get("nota"):
 			registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
 	return render_template("sobre.html", registros=registros)
+
+@app.route('/filmes/<parametros>')
+def filmes(parametros):
+
+	if parametros == 'populares':
+		page_html = 'filmespopulares.html'
+		url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=74fdabc28b53a078461d75e8cd04eb28'
+	if parametros == 'top_rated':
+		page_html = 'filmetoprated.html'
+		url = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=200&api_key=74fdabc28b53a078461d75e8cd04eb28'
+
+	response = urllib.request.urlopen(url)
+
+	dados = response.read()
+
+	jsondata = json.loads(dados)
+
+	return render_template(page_html, filmes=jsondata['results'])
 
 if __name__ == '__main__':
 	app.run(debug=True)		
